e1cb38a9c55fc2562649773b1640f2ec49e35f44
Olga Fursova <olga.fursova.zp@gmail.com>
2025-06-20 14:09:35 +0300

    update: added user and subscription contexts to app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Index from "@/pages/Index";
 import Auth from "@/pages/Auth";
 import Questionnaire from "@/pages/Questionnaire";
@@ -15,33 +16,43 @@ import { ResetPassword } from "@/pages/ResetPassword";
 import { UpdatePassword } from "@/pages/UpdatePassword";
 import PricingPage from "./pages/PricingPage";
 import PaymentSuccess from "./pages/PaymentSuccess";
+import { UserProvider } from "./contexts/UserContext";
+import { SubscriptionProvider } from "./contexts/SubscriptionContext";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1 bg-gray-50">
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/update-password" element={<UpdatePassword />} />
-            <Route path="/pricing" element={<PricingPage />} />
-            <Route path="/payment-success" element={<PaymentSuccess />} />
-            <Route path="/questionnaire" element={<Questionnaire />} />
-            <Route path="/questionnaire-confirmation" element={<QuestionnaireConfirmation />} />
-            <Route path="/generate-documents" element={<GenerateDocuments />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/project/:projectId" element={<ProjectDetails />} />
-            <Route path="/access-details" element={<AccessDetails />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-      <Toaster />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <SubscriptionProvider>
+          <Router>
+            <div className="min-h-screen flex flex-col">
+              <Navbar />
+              <main className="flex-1 bg-gray-50">
+                <Routes>
+                  <Route path="/" element={<Index />} />
+                  <Route path="/auth" element={<Auth />} />
+                  <Route path="/reset-password" element={<ResetPassword />} />
+                  <Route path="/update-password" element={<UpdatePassword />} />
+                  <Route path="/pricing" element={<PricingPage />} />
+                  <Route path="/payment-success" element={<PaymentSuccess />} />
+                  <Route path="/questionnaire" element={<Questionnaire />} />
+                  <Route path="/questionnaire-confirmation" element={<QuestionnaireConfirmation />} />
+                  <Route path="/generate-documents" element={<GenerateDocuments />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/project/:projectId" element={<ProjectDetails />} />
+                  <Route path="/access-details" element={<AccessDetails />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+            <Toaster />
+          </Router>
+        </SubscriptionProvider>
+      </UserProvider>
+    </QueryClientProvider>
   );
 }
 
